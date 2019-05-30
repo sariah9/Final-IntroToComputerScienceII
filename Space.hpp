7bfe3d24fb186d0073c48da72059344e4ffd0a2e
Sariah Bunnell <33461825+sariah9@users.noreply.github.com>
2019-05-30 09:35:43 -0700

    Update Space.hpp

--- Space.hpp
+++ Space.hpp
@@ -1,1 +1,6 @@
-
+/*********************************************************************
+** Program name: Final Project - Collect Three Loot and Shoot
+** Author: Sariah Bunnell 
+** Date:  May 30 2019
+** Description: Board header file 
+*********************************************************************/
