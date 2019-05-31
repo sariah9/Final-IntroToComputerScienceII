30be29709594cdb1e919cfa10f740431362895cf
Sariah Bunnell <33461825+sariah9@users.noreply.github.com>
2019-05-31 11:33:56 -0700

    Update Board.hpp

--- Board.hpp
+++ Board.hpp
@@ -2,7 +2,7 @@
 ** Program name: Final Project - Triad Town Loot and Shoot
 ** Author: Sariah Bunnell 
 ** Date:  May 30 2019
-** Description: Board header file 
+** Description: Board class header file 
 *********************************************************************/
 #ifndef BOARD_HPP
 #define BOARD_HPP
@@ -10,9 +10,25 @@
 class Board
 {
 private:
-  gameBoard[12][12];
+  map[9][12];
+  Space* mtnPtr;
+  Space* forestPtr;
+  Space* coastPtr;
+  queue<char> knapSack;
 public:
+  Board();
+  ~Board();
+  void moveUser();
   void boardPopulate();
+  void displayNine();
+  void displayTwFive();
+  void printHidden();
+  void printFull();
+  void randomOffer();
+  void printSack();
+  void deleteItem();
+  void feedBears();
+  void keepOrDropMenu();
 };
 
 #endif
