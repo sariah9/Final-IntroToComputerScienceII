d7dc059cf5699cac488ba21d80df70cacb52fd77
Sariah Bunnell <33461825+sariah9@users.noreply.github.com>
2019-05-31 11:51:55 -0700

    Create Coast.cpp

---
+++ Coast.cpp
@@ -0,0 +1,7 @@
+/*********************************************************************
+** Program name: Final Project - Triad Town Loot and Shoot
+** Author: Sariah Bunnell 
+** Date:  May 30 2019
+** Description: Coast derived class implementation file 
+*********************************************************************/
+#include "Coast.hpp"
