4c3cc8d7119f2236c348f21d3d97c40ae0ee5302
Sariah Bunnell <33461825+sariah9@users.noreply.github.com>
2019-05-31 11:40:52 -0700

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -1,3 +1,9 @@
+/*********************************************************************
+** Program name: Final Project - Triad Town Loot and Shoot
+** Author: Sariah Bunnell 
+** Date:  May 30 2019
+** Description: Board class implementation file 
+*********************************************************************/
 #include "Board.hpp"
 
 Board::Board()
