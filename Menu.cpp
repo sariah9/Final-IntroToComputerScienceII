67359474a1fd27ac25783c388196b3b257f0d070
Sariah Bunnell <33461825+sariah9@users.noreply.github.com>
2019-05-31 11:33:00 -0700

    Update Menu.cpp

--- Menu.cpp
+++ Menu.cpp
@@ -1,3 +1,9 @@
+/*********************************************************************
+** Program name: Final Project - Triad Town Loot and Shoot
+** Author: Sariah Bunnell 
+** Date:  May 30 2019
+** Description:  Menu class implementation file 
+*********************************************************************/
 #include "Menu.hpp"
 
 Menu::Menu()
