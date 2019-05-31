5dce0402c95aca2019539c8cada317446618ffce
Sariah Bunnell <33461825+sariah9@users.noreply.github.com>
2019-05-31 11:50:02 -0700

    Create Forest.cpp

---
+++ Forest.cpp
@@ -0,0 +1,7 @@
+/*********************************************************************
+** Program name: Final Project - Triad Town Loot and Shoot
+** Author: Sariah Bunnell 
+** Date:  May 30 2019
+** Description: Forest derived class implementation file 
+*********************************************************************/
+#include "Forest.hpp"
