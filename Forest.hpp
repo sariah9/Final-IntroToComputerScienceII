d0f4962d84f338654caddaaa9585b7eccc795a3b
Sariah Bunnell <33461825+sariah9@users.noreply.github.com>
2019-05-31 11:49:09 -0700

    Create Forest.hpp

---
+++ Forest.hpp
@@ -0,0 +1,16 @@
+/*********************************************************************
+** Program name: Final Project - Triad Town Loot and Shoot
+** Author: Sariah Bunnell 
+** Date:  May 30 2019
+** Description: Forest derived class  header file 
+*********************************************************************/
+#ifndef FOREST_HPP
+#define FOREST_HPP
+
+class Forest : public Space
+{
+private:
+public:
+};
+
+#endif
