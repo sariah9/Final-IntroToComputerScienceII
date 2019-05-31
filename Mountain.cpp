cb134347735fb5fb793f81256f44adc54d33e567
Sariah Bunnell <33461825+sariah9@users.noreply.github.com>
2019-05-31 11:48:03 -0700

    Create Mountain.cpp

---
+++ Mountain.cpp
@@ -0,0 +1,8 @@
+/*********************************************************************
+** Program name: Final Project - Triad Town Loot and Shoot
+** Author: Sariah Bunnell 
+** Date:  May 30 2019
+** Description: Mountain derived class implementation file 
+*********************************************************************/
+#include "Mountain.hpp"
+
