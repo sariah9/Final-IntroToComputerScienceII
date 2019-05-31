eef89393c45214a08ec83bbb5c360b85d0124854
Sariah Bunnell <33461825+sariah9@users.noreply.github.com>
2019-05-31 11:27:05 -0700

    Update Space.cpp

--- Space.cpp
+++ Space.cpp
@@ -1,6 +1,22 @@
 /*********************************************************************
-** Program name: Final Project - Collect Three Loot and Shoot
+** Program name: Final Project - Triad Town Loot and Shoot
 ** Author: Sariah Bunnell 
 ** Date:  May 30 2019
 ** Description: Board header file 
 *********************************************************************/
+#include "Space.hpp"
+
+Space::Space()
+{
+up = NULL;
+down = NULL;
+right = NULL;
+left = NULL;
+}
+Space::~Space()
+{
+up = NULL;
+down = NULL;
+right = NULL;
+left = NULL;
+}
