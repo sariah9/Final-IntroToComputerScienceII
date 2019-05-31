57a4a51d3befe8a262c0a95772efbd87c75c3d48
Sariah Bunnell <33461825+sariah9@users.noreply.github.com>
2019-05-31 11:39:23 -0700

    Update Space.hpp

--- Space.hpp
+++ Space.hpp
@@ -2,7 +2,7 @@
 ** Program name: Final Project - Triad Town Loot and Shoot
 ** Author: Sariah Bunnell 
 ** Date:  May 30 2019
-** Description: Board header file 
+** Description: Space class header file 
 *********************************************************************/
 #ifndef SPACE_HPP
 #define SPACE_HPP
