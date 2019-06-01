8d978eec55c019b1829ab8b806802841707f0c4f
Sariah Bunnell <33461825+sariah9@users.noreply.github.com>
2019-05-31 22:10:13 -0700

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -29,6 +29,55 @@ Board::~Board()
 ** Parameters:
 ** Returns: 
 *********************************************************************/
+-constructor
+  -STL barrel of food
+  -STL barrel of wood
+  -STL barrel of sailors
+  Space* user //keeps track of user
+-destructor 
+  -.pop all barrels STL
+-moveUser()
+  -need a way to keep track of where user is... 
+-displayNine()
+  -prints out linked 3x3 grid 
+-printHidden()
+  -prints faux grid with only the path
+  -or whole board with blank spaces
+-printFull()
+  -used for testing and at the end
+  -prints whole board that is correct
+-printSack()
+  -display items in knapSack
+  -traverse queue front to back
+-deleteItem(int chosen)
+  -delete specific item depending on parameter
+  -use searching method in book
+  -used when user drops item
+  -maybe offer delete item too?
+-feedBears()
+  -call deleteItem -parameter salmon
+  -delete bear characters
+-offerItem(int item)
+  -depending on argument, offer enumerated item
+  -if yes
+    -keep
+  -if no
+    -drop()
+-keep()
+  -enqueue item into knapsack queue
+-drop()
+  -check surrounding grid for 2+ of item
+  -if yes
+    -offerItem(int item)
+-foodBarrels()
+  -STL queue of trout barrels
+  -if yes .push
+-woodBarrels()
+  -STL queue of trout barrels
+  -if yes .push
+-sailorBarrels()
+  -STL queue of trout barrels
+  -if yes .push
 void Board::moveUser()
 {
 }
