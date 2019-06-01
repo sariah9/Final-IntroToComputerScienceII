a076fc136a03346a3f907f855023a5ffc7867564
Sariah Bunnell <33461825+sariah9@users.noreply.github.com>
2019-05-31 22:11:12 -0700

    Update Coast.cpp

--- Coast.cpp
+++ Coast.cpp
@@ -11,3 +11,89 @@
 ** Parameters:
 ** Returns: 
 *********************************************************************/
+void Coast::words()
+{
+  cout << "Finally! The coast is here!" << endl;
+  cout << "You can already smell the salty \n"
+       << "tang of the ocean spray and hear \n"
+       << "sea gulls as they fight over \n"
+       << "crab. Under a sunny, cloudless \n"
+       << "sky, you begin to look for sailors. " << endl;
+}
+void Coast::randomOffer()
+{
+  int random = (rand() % 4) + 1;
+  if (random == 1)
+  {
+    //sailor
+  }
+  else if (random == 2)
+  {
+    //egg
+  }
+  else if (random == 3)
+  {
+    //extra space
+  }
+  else 
+  {
+     //sailor
+  }
+}
+void Coast::boardPopulate(int move)
+{
+  const char bear = '&';
+  const char path = ' ';
+  const char sailor = '+';
+  const char egg = 'o';
+  if (move == 1)
+  {
+    gridMove[2][1] = path;
+    gridMove[1][1] = path;
+    gridMove[1][0] = path;
+    for (int y = 0; y < 2; y++)
+    {
+      int random = (rand() % 3) + 1;
+      if (random == 1)
+      { 
+        gridMove[y][2] = bear;
+      }
+      else if (random == 2)
+      {
+        gridMove[y][2] = egg;
+      }
+      else 
+      {
+        gridMove[y][2] = sailor;
+      }
+    }
+    gridMove[0][0] = egg;
+    gridMove[2][0] = sailor;
+    gridMove[0][1] = sailor;
+  }
+  else 
+  {
+    gridMove[1][0] = path;
+    gridMove[1][1] = path;
+    gridMove[1][2] = path;
+    for (int y = 0; y < 2; y += 2)
+    {
+      for (int x = 0; x < 2; x++)
+      {
+        int random = (rand() % 3) + 1;
+        if (random == 1)
+        { 
+          gridMove[y][x] = bear;
+        }
+        else if (random == 2)
+        {
+          gridMove[y][x] = egg;
+        }
+        else 
+        {
+          gridMove[y][x] = sailor;
+        }
+      }
+    }
+  }
+}
