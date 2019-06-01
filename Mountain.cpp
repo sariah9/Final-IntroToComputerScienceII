2ffb73622a4811e769ef965826db7717d0beb49a
Sariah Bunnell <33461825+sariah9@users.noreply.github.com>
2019-05-31 22:13:08 -0700

    Update Mountain.cpp

--- Mountain.cpp
+++ Mountain.cpp
@@ -11,3 +11,87 @@
 ** Parameters:
 ** Returns: 
 *********************************************************************/
+void Mountain::words()
+{
+  cout << "Here you are in the Mountains! " << endl;
+  cout << "Ah, the fresh air, the stunning peaks, \n"
+       << "the sounds of waterfalls and eagles!" << endl;
+  cout << "A perfect day for a trout hunt. " << endl;
+}
+void Mountain::randomOffer()
+{
+  int random = (rand() % 4) + 1;
+  if (random == 1)
+  {
+    //trout
+  }
+  else if (random == 2)
+  {
+    //egg
+  }
+  else if (random == 3)
+  {
+    //flashlight
+  }
+  else 
+  {
+    //trout
+  }
+}
+void Mountain::boardPopulate(int move)
+{
+  const char bear = '&';
+  const char path = ' ';
+  const char trout = '%';
+  const char egg = 'o';
+  if (move == 4)
+  {
+    gridMove[0][1] = path;
+    gridMove[1][1] = path;
+    gridMove[1][2] = path;
+    for (int y = 0; y < 2; y++)
+    {
+      int random = (rand() % 3) + 1;
+      if (random == 1)
+      { 
+        gridMove[y][0] = bear;
+      }
+      else if (random == 2)
+      {
+        gridMove[y][0] = egg;
+      }
+      else 
+      {
+        gridMove[y][0] = trout;
+      }
+    }
+    gridMove[0][2] = bear;
+    gridMove[2][1] = trout;
+    gridMove[2][2] = trout;
+  }
+  else 
+  {
+    gridMove[1][0] = path;
+    gridMove[1][1] = path;
+    gridMove[1][2] = path;
+    for (int y = 0; y < 2; y += 2)
+    {
+      for (int x = 0; x < 2; x++)
+      {
+        int random = (rand() % 3) + 1;
+        if (random == 1)
+        { 
+          gridMove[y][x] = bear;
+        }
+        else if (random == 2)
+        {
+          gridMove[y][x] = egg;
+        }
+        else 
+        {
+          gridMove[y][x] = trout;
+        }
+      }
+    }
+  }
+}
