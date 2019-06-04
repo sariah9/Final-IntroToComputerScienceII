9aecc23e1bad39de69beada57a3f014267ee63f3
Sariah Bunnell <33461825+sariah9@users.noreply.github.com>
2019-06-04 11:56:36 -0700

    Update Forest.cpp

--- Forest.cpp
+++ Forest.cpp
@@ -21,32 +21,6 @@ void Forest::words()
        << "any wood that may be near. " << endl;
 }
 /*********************************************************************
-** Function: randomOffer
-** Description: randomly generates items for the Forest level
-** Parameters: None
-** Returns: int that represents item
-*********************************************************************/
-int Forest::randomOffer()
-{
-  int random = (rand() % 4) + 1;
-  if (random == 1)
-  {
-    return 1;//wood
-  }
-  else if (random == 2)
-  {
-    return 2;//egg
-  }
-  else if (random == 3)
-  {
-    return 3;//crystal
-  }
-  else 
-  {
-    return 4;//wood
-  }
-}
-/*********************************************************************
 ** Function: boardPopulate
 ** Description: randomly fills 3 x 3 grid with pathway and items and 
 ** bears
@@ -70,19 +44,25 @@ void Forest::boardPopulate(int move)
       if (random == 1)
       { 
         gridMove[0][x] = bear;
+        bearCount++;
       }
       else if (random == 2)
       {
         gridMove[0][x] = egg;
+        eggCount++;
       }
       else 
       {
         gridMove[0][x] = wood;
+        itemCount++;
       }
     }
     gridMove[1][0] = bear;
+    bearCount++;
     gridMove[2][0] = wood;
+    itemCount++;
     gridMove[2][2] = wood;
+    itemCount++;
   }
   else if (move == 4)
   {
@@ -95,19 +75,25 @@ void Forest::boardPopulate(int move)
       if (random == 1)
       { 
         gridMove[y][2] = bear;
+        bearCount++;
       }
       else if (random == 2)
       {
         gridMove[y][2] = egg;
+        eggCount++;
       }
       else 
       {
         gridMove[y][2] = wood;
+        itemCount++;
       }
     }
     gridMove[0][0] = egg;
+    eggCount++;
     gridMove[2][1] = wood;
+    itemCount++;
     gridMove[2][0] = wood;
+    itemCount++;
   }
   else 
   {
@@ -122,16 +108,32 @@ void Forest::boardPopulate(int move)
         if (random == 1)
         { 
           gridMove[y][x] = bear;
+          bearCount++;
         }
         else if (random == 2)
         {
           gridMove[y][x] = egg;
+          eggCount++;
         }
         else 
         {
           gridMove[y][x] = wood;
+          itemCount++;
         }
       }
     }
   }
+}
+
+int Forest::getEggs()
+{
+  return eggCount;
+}
+int Forest::getItems()
+{
+  return itemCount;
+}
+int Forest::getBears()
+{
+  return bearCount;
 }
