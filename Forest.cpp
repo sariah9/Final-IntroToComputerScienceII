270eba34dc1008c662432702862611357348d44e
Sariah Bunnell <33461825+sariah9@users.noreply.github.com>
2019-06-07 14:22:04 -0700

    Update Forest.cpp

--- Forest.cpp
+++ Forest.cpp
@@ -192,7 +192,7 @@ void Forest::boardPopulate(int move)
   }
   for (int y = 0; y < 2; y++)
   {
-    for (int x = 0; x < 2; x++}
+    for (int x = 0; x < 2; x++)
     {
       cout << gridMove[y][x];
     }
