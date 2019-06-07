d998237098e65fcfd6d02cedead44e995444a812
Sariah Bunnell <33461825+sariah9@users.noreply.github.com>
2019-06-07 14:21:42 -0700

    Update Coast.cpp

--- Coast.cpp
+++ Coast.cpp
@@ -141,7 +141,7 @@ void Coast::boardPopulate(int move)
   }
   for (int y = 0; y < 2; y++)
   {
-    for (int x = 0; x < 2; x++}
+    for (int x = 0; x < 2; x++)
     {
       cout << gridMove[y][x];
     }
