0e454d735e8889f79685c4d6aa06131ee6b543bc
Sariah Bunnell <33461825+sariah9@users.noreply.github.com>
2019-06-08 09:40:25 -0700

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -672,3 +672,183 @@ void Board::beginPlay(int level)
     addToSack(7);
   }
 }
+/*********************************************************************
+** Function: boardPopulate
+** Description: randomly fills 3 x 3 grid with pathway and items and 
+** bears
+** Parameters: int that represents which move the player is on
+** Returns: None
+*********************************************************************/
+void Board::boardPopulate(int move)
+{
+  const char bear = '&';
+  const char path = ' ';
+  const char wood = '#';
+  const char egg = 'o';
+  eggCount = 0;
+  itemCount = 0;
+  bearCount = 0;
+  int random = (rand() % 3) + 1;
+  if  (move == 7)
+  {
+    gridMove[2][1] = path;
+    gridMove[1][1] = path;
+    gridMove[0][1] = path;
+    if (bearImmunity())
+    { 
+      gridMove[0][0] = wood;
+      itemCount++;
+      gridMove[2][1] = egg;
+      eggCount++;
+      gridMove[2][0] = wood;
+      itemCount++;
+      gridMove[2][2] = egg;
+      eggCount++;
+      gridMove[1][2] = wood;
+      itemCount++;
+      gridMove[0][2] = egg;
+      eggCount++;
+    }
+    else if (random == 2)
+    {
+      gridMove[0][0] = bear;
+      bearCount++;
+      gridMove[2][1] = egg;
+      eggCount++;
+      gridMove[2][0] = wood;
+      itemCount++;
+      gridMove[2][2] = egg;
+      eggCount++;
+      gridMove[1][2] = wood;
+      itemCount++;
+      gridMove[0][2] = bear;
+      bearCount++;
+    }
+    else 
+    {
+      gridMove[0][0] = egg;
+      eggCount++;
+      gridMove[2][1] = egg;
+      eggCount++;
+      gridMove[2][0] = bear;
+      bearCount++;
+      gridMove[1][2] = wood;
+      itemCount++;
+      gridMove[2][2] = wood;
+      itemCount++;
+      gridMove[0][2] = bear;
+      bearCount++;
+    }
+  }
+  else if (move == 4)
+  {
+    gridMove[2][1] = path;
+    gridMove[1][1] = path;
+    gridMove[1][2] = path;
+    if (bearImmunity())
+    { 
+      gridMove[0][0] = wood;
+      itemCount++;
+      gridMove[0][1] = wood;
+      itemCount++;
+      gridMove[1][0] = wood;
+      itemCount++;
+      gridMove[2][0] = egg;
+      eggCount++;
+      gridMove[2][2] = egg;
+      eggCount++;
+      gridMove[0][2] = egg;
+      eggCount++;
+    }
+    else if (random == 3)
+    {
+      gridMove[0][0] = egg;
+      eggCount++;
+      gridMove[0][1] = egg;
+      eggCount++;
+      gridMove[2][0] = bear;
+      bearCount++;
+      gridMove[1][0] = wood;
+      itemCount++;
+      gridMove[2][2] = wood;
+      itemCount++;
+      gridMove[0][2] = bear;
+      bearCount++;
+    }
+    else
+    {
+      gridMove[0][0] = wood;
+      itemCount++;
+      gridMove[0][1] = egg;
+      eggCount++;
+      gridMove[2][0] = bear;
+      bearCount++;
+      gridMove[1][0] = wood;
+      itemCount++;
+      gridMove[2][2] = wood;
+      itemCount++;
+      gridMove[0][2] = bear;
+      bearCount++;
+    }
+  }
+  else
+  {
+    gridMove[1][1] = path;
+    gridMove[1][0] = path;
+    gridMove[1][2] = path;
+    if (bearImmunity())
+    { 
+      gridMove[0][0] = wood;
+      itemCount++;
+      gridMove[0][1] = wood;
+      itemCount++;
+      gridMove[2][0] = wood;
+      itemCount++;
+      gridMove[2][1] = egg;
+      eggCount++;
+      gridMove[2][2] = egg;
+      eggCount++;
+      gridMove[0][2] = egg;
+      eggCount++;
+    }
+    else if (random == 1)
+    {
+      gridMove[0][0] = egg;
+      eggCount++;
+      gridMove[0][1] = egg;
+      eggCount++;
+      gridMove[2][0] = bear;
+      bearCount++;
+      gridMove[2][1] = wood;
+      itemCount++;
+      gridMove[2][2] = wood;
+      itemCount++;
+      gridMove[0][2] = bear;
+      bearCount++;
+    }
+    else
+    {
+      gridMove[0][0] = wood;
+      itemCount++;
+      gridMove[0][1] = egg;
+      eggCount++;
+      gridMove[2][0] = bear;
+      bearCount++;
+      gridMove[2][1] = wood;
+      itemCount++;
+      gridMove[2][2] = wood;
+      itemCount++;
+      gridMove[0][2] = bear;
+      bearCount++;
+    }
+  }
+  for (int y = 0; y < 2; y++)
+  {
+    for (int x = 0; x < 2; x++)
+    {
+      cout << gridMove[y][x] << " ";
+    }
+    cout << endl;
+  }
+  cout << endl;
+}
